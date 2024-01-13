4932520261a702f235a29f8cbbaac2c6abb64206
Deyan Delchev <60808936+lastvoidtemplar@users.noreply.github.com>
2024-01-13 12:33:39 +0200

    add cascade delete and fix genre type

--- migrations/20231227171635_create_table_books.sql
+++ migrations/20231227171635_create_table_books.sql
@@ -6,27 +6,27 @@ CREATE TABLE IF NOT EXISTS Books (
     title VARCHAR(100)  NOT NULL,
     date_of_publication DATE,
     plot VARCHAR(5000),
-    genre INTEGER
+    genre VARCHAR(50)
 );
 
 CREATE TABLE IF NOT EXISTS BookPageRatings (
     book_rating_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    book_id UUID REFERENCES Books(book_id),
+    book_id UUID REFERENCES Books(book_id)  ON DELETE CASCADE,
     rating_type INTEGER,
     user_id UUID
 );
 
 CREATE TABLE IF NOT EXISTS BookReviews (
     book_reviews_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    root_id UUID REFERENCES BookReviews(book_reviews_id),
-    book_id UUID REFERENCES Books(book_id),
+    root_id UUID REFERENCES BookReviews(book_reviews_id)  ON DELETE CASCADE,
+    book_id UUID REFERENCES Books(book_id)  ON DELETE CASCADE,
     user_id UUID  NOT NULL,
     content VARCHAR(1000)  NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS AuthorsBooks (
-    author_id UUID NOT NULL REFERENCES Author(author_id),
-    book_id UUID NOT NULL REFERENCES Books(book_id),
+    author_id UUID NOT NULL REFERENCES Author(author_id)  ON DELETE CASCADE,
+    book_id UUID NOT NULL REFERENCES Books(book_id)  ON DELETE CASCADE,
     PRIMARY KEY(author_id, book_id)
 );
 -- +goose StatementEnd
